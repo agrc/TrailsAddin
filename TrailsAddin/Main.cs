e5cdd294744f721db29561d1ff8647355f73deda
Scott Davis <stdavis@utah.gov>
2017-12-04 16:54:30 -0700

    Add and combine build on select checkbox.

--- TrailsAddin/Main.cs
+++ TrailsAddin/Main.cs
@@ -11,6 +11,8 @@ using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Core.Geometry;
 using System.Collections.Generic;
 using static ArcGIS.Desktop.Editing.EditOperation;
+using ArcGIS.Desktop.Mapping.Events;
+using System.Diagnostics;
 
 namespace TrailsAddin
 {
@@ -22,8 +24,11 @@ namespace TrailsAddin
         public static FeatureLayer SegmentsLayer;
         public static FeatureLayer TrailheadsLayer;
         public static StandaloneTable RoutesStandaloneTable;
-        public static List<Row[]> Parts = new List<Row[]>();
-        public static event EventHandler<OnAddPartArgs> RaiseOnAddPart;
+        private static FeatureLayer TempSegmentsLayer;
+        public static bool BuildOnSelect = false;
+        private static List<string> tempSegmentIDs = new List<string>();
+        private static int currentPart = 1;
+        private static FeatureLayer USNGLayer;
 
         /// <summary>
         /// Retrieve the singleton instance to this module here
@@ -43,10 +48,72 @@ namespace TrailsAddin
             SGIDTrailsLayer = GetLayer("SGID10.RECREATION.Trails");
             SegmentsLayer = GetLayer("TrailSegments");
             TrailheadsLayer = GetLayer("Trailheads");
+            TempSegmentsLayer = GetLayer("Temp Segments");
+            USNGLayer = GetLayer("SGID10.INDICES.NationalGrid");
             RoutesStandaloneTable = MapView.Active.Map.StandaloneTables.First(l => l.Name == "Routes") as StandaloneTable;
+
+            MapSelectionChangedEvent.Subscribe((MapSelectionChangedEventArgs args) =>
+            {
+                if (BuildOnSelect && args.Selection.Keys.Contains(SGIDTrailsLayer as MapMember))
+                {
+                    AddSelectedToTemp();
+                }
+            });
         }
 
-        private static FeatureLayer GetLayer(string name)
+        private static void AddSelectedToTemp()
+        {
+            QueuedTask.Run(() =>
+            {
+                using (FeatureClass tempSegsFC = TempSegmentsLayer.GetFeatureClass())
+                using (RowCursor sgidSegs = SGIDTrailsLayer.GetSelection().Search(null, false))
+                {
+                    EditOperation operation = new EditOperation();
+                    operation.Name = "add selected to temp segments";
+                    bool newPartCreated = false;
+                    operation.Callback(context =>
+                    {
+                        while (sgidSegs.MoveNext())
+                        {
+                            string id = GetUSNGID_Line(sgidSegs.Current);
+                            RowBuffer tempRowBuf = CopyRowValues(sgidSegs.Current, tempSegsFC);
+                            tempRowBuf["USNG_SEG"] = id;
+                            if (tempSegmentIDs.Contains(id))
+                            {
+                                currentPart++;
+                                tempSegmentIDs.Clear();
+                                newPartCreated = true;
+                            }
+                            tempSegmentIDs.Add(id);
+                            tempRowBuf["RoutePart"] = currentPart;
+                            tempSegsFC.CreateRow(tempRowBuf);
+                            tempRowBuf.Dispose();
+                        }
+                        context.Invalidate(tempSegsFC);
+                    }, tempSegsFC);
+                    if (newPartCreated)
+                    {
+                        // NOT WORKING ref: https://community.esri.com/message/733381-re-examples-for-setonundone-setonredone-setoncomitted?commentID=733381#comment-733381
+                        operation.SetOnUndone(() =>
+                        {
+                            currentPart--;
+                        });
+                        operation.SetOnRedone(() =>
+                        {
+                            currentPart++;
+                        });
+                    }
+
+                    bool success = operation.Execute();
+                    if (success)
+                    {
+                        SGIDTrailsLayer.ClearSelection();
+                    }
+                }
+            });
+        }
+
+        internal static FeatureLayer GetLayer(string name)
         {
             return MapView.Active.Map.GetLayersAsFlattenedList().First(l => l.Name == name) as FeatureLayer;
         }
@@ -74,9 +141,15 @@ namespace TrailsAddin
                 using (Geodatabase geodatabase = SegmentsLayer.GetTable().GetDatastore() as Geodatabase)
                 using (AttributedRelationshipClass segmentsRelationshipClass = geodatabase.OpenDataset<AttributedRelationshipClass>("RouteToTrailSegments"))
                 using (RelationshipClass trailheadsRelationshipClass = geodatabase.OpenDataset<RelationshipClass>("TrailheadToRoute"))
+                using (FeatureClass tempSegsFC = TempSegmentsLayer.GetFeatureClass())
                 {
-                    // TODO: QA/QC route name unique, ordered segments are connected
+                    if (!BuildOnSelect && SGIDTrailsLayer.SelectionCount == 0)
+                    {
+                        MessageBox.Show("At least one segment must be selected!");
+                        return;
+                    }
 
+                    // TODO: QA/QC route name unique
                     var operation = new EditOperation();
                     operation.Name = "Create new trails route: " + routeName;
 
@@ -85,11 +158,43 @@ namespace TrailsAddin
                         routeBuf["RouteName"] = routeName;
                         Row routeRow = routesTable.CreateRow(routeBuf);
 
+                        if (BuildOnSelect)
+                        {
+                            // get segments from TempSegments layer
+                            bool atLeastOne = false;
+                            using (RowCursor tempSegsCursor = tempSegsFC.Search(null, false))
+                            {
+                                while (tempSegsCursor.MoveNext())
+                                {
+                                    atLeastOne = true;
+                                    Row row = tempSegsCursor.Current;
+                                    CopySegment(row, (short)row["RoutePart"], segmentsFeatureClass, segmentsRelationshipClass, context, routeRow);
+                                }
+                                Reset();
+                                tempSegsFC.DeleteRows(new QueryFilter());
+                                context.Invalidate(tempSegsFC);
+                            }
+
+                            if (!atLeastOne)
+                            {
+                                context.Abort("There must be at least one feature in TempSegments!");
+                            }
+                        }
+                        else
+                        {
+                            // get segments from selected features
+                            while (trailsSelectionCursor.MoveNext())
+                            {
+                                CopySegment(trailsSelectionCursor.Current, 1, segmentsFeatureClass, segmentsRelationshipClass, context, routeRow);
+                            }
+                        }
+
+                        // trailhead
                         if (SGIDTrailheadsLayer.SelectionCount == 1)
                         {
                             trailheadsSelectionCursor.MoveNext();
                             RowBuffer trailheadRowBuf = CopyRowValues(trailheadsSelectionCursor.Current, trailheadsFeatureClass);
-                            trailheadRowBuf["USNG_TH"] = Guid.NewGuid().ToString().Substring(0, 13);
+                            trailheadRowBuf["USNG_TH"] = GetUSNGID_Point(((Feature)trailheadsSelectionCursor.Current).GetShape() as MapPoint);
                             routeRow["THID_FK"] = trailheadRowBuf["USNG_TH"];
                             using (Row trailheadRow = trailheadsFeatureClass.CreateRow(trailheadRowBuf))
                             {
@@ -97,36 +202,13 @@ namespace TrailsAddin
                             }
                         }
 
-
-                        int partNumber = 1;
-                        if (Parts.Count > 0)
-                        {
-                            Parts.ForEach(part =>
-                            {
-                                foreach (Row row in part)
-                                {
-                                    // copy segments and populate routePart as partNumber
-                                    CopySegment(row, partNumber, segmentsFeatureClass, segmentsRelationshipClass, context, routeRow);
-                                }
-                                partNumber++;
-                            });
-                        }
-
-                        while (trailsSelectionCursor.MoveNext())
-                        {
-                            CopySegment(trailsSelectionCursor.Current, partNumber, segmentsFeatureClass, segmentsRelationshipClass, context, routeRow);
-                        }
-
                         context.Invalidate(routeRow);
                         routeRow.Dispose();
-                    }, routesTable, segmentsFeatureClass, segmentsRelationshipClass);
+                    }, routesTable, segmentsFeatureClass, segmentsRelationshipClass, tempSegsFC);
 
                     operation.Execute();
                     if (operation.IsSucceeded)
                     {
-                        Parts.Clear();
-                        RaiseOnAddPart(_this, new OnAddPartArgs(Parts.Count));
-
                         Notification notification = new Notification();
                         notification.Title = FrameworkApplication.Title;
                         notification.Message = string.Format("Route: \"{0}\" added successfully!", routeName);
@@ -142,10 +224,10 @@ namespace TrailsAddin
             });
         }
 
-        private static void CopySegment (Row row, int partNum, FeatureClass segmentsFeatureClass, AttributedRelationshipClass segmentsRelationshipClass, IEditContext context, Row routeRow) {
+        private static void CopySegment (Row row, Int16 partNum, FeatureClass segmentsFeatureClass, AttributedRelationshipClass segmentsRelationshipClass, IEditContext context, Row routeRow) {
             RowBuffer segRowBuf = CopyRowValues(row, segmentsFeatureClass);
 
-            segRowBuf["USNG_SEG"] = Guid.NewGuid().ToString().Substring(0, 13);
+            segRowBuf["USNG_SEG"] = GetUSNGID_Line(row);
 
             using (Row segRow = segmentsFeatureClass.CreateRow(segRowBuf))
             {
@@ -154,16 +236,15 @@ namespace TrailsAddin
                 relationshipRowBuf["RoutePart"] = partNum;
                 segmentsRelationshipClass.CreateRelationship(routeRow, segRow, relationshipRowBuf);
             }
-
         }
 
-        private static RowBuffer CopyRowValues(Row originRow, FeatureClass destinationFeatureClass)
+        internal static RowBuffer CopyRowValues(Row originRow, FeatureClass destinationFeatureClass)
         {
             RowBuffer segRowBuf = destinationFeatureClass.CreateRowBuffer();
 
             foreach (Field field in originRow.GetFields())
             {
-                if (field.IsEditable)
+                if (field.IsEditable && field.Name != "RoutePart")
                 {
                     segRowBuf[field.Name] = originRow[field.Name];
                 }
@@ -172,47 +253,91 @@ namespace TrailsAddin
             return segRowBuf;
         }
 
-        internal static bool CanOnAddPartButtonClick
+        private static string GetUSNGID_Point(MapPoint point)
         {
-            get {
-                return SGIDTrailsLayer.SelectionCount > 0;
-            }
-        }
-
-        internal static async void OnAddPartButtonClick()
-        {
-            await QueuedTask.Run(() =>
+            SpatialQueryFilter filter = new SpatialQueryFilter()
             {
-                Row[] rows = new Row[SGIDTrailsLayer.SelectionCount];
-                using (RowCursor trailsSelectionCursor = SGIDTrailsLayer.GetSelection().Search(null, false))
-                {
-                    int count = 0;
-                    while (trailsSelectionCursor.MoveNext())
-                    {
-                        rows[count] = trailsSelectionCursor.Current;
-                        count++;
-                    }
-                }
-                Parts.Add(rows);
+                FilterGeometry = point,
+                SpatialRelationship = SpatialRelationship.Intersects,
+                SubFields = "GRID1MIL,GRID100K"
+            };
+            RowCursor cursor = USNGLayer.Search(filter);
+            cursor.MoveNext();
+            Row row = cursor.Current;
+            string grid1mil = (string)row["GRID1MIL"];
+            string grid100k = (string)row["GRID100K"];
+            cursor.Dispose();
 
-                RaiseOnAddPart(_this, new OnAddPartArgs(Parts.Count));
+            // this code is from gregs roads code: https://gist.github.com/gregbunce/1733a741d8b4343a7a60fc42acf5086b
+            double dblMeterX = (double)point.X;
+            double dblMeterY = (double)point.Y;
+            // add .5 to so when we conver to long and the value gets truncated, it will still regain our desired value (if you need more info on this, talk to Bert)
+            dblMeterX = dblMeterX + .5;
+            dblMeterY = dblMeterY + .5;
+            long lngMeterX = (long)dblMeterX;
+            long lngMeterY = (long)dblMeterY;
 
-                SGIDTrailsLayer.ClearSelection();
-            });
+            // trim the x and y meter values to get the needed four characters from each value
+            string strMeterX_NoDecimal = lngMeterX.ToString();
+            string strMeterY_NoDecimal = lngMeterY.ToString();
+
+            // remove the begining characters
+            strMeterX_NoDecimal = strMeterX_NoDecimal.Remove(0, 1);
+            strMeterY_NoDecimal = strMeterY_NoDecimal.Remove(0, 2);
+
+            //remove the ending characters
+            strMeterY_NoDecimal = strMeterY_NoDecimal.Remove(strMeterY_NoDecimal.Length - 1);
+            strMeterX_NoDecimal = strMeterX_NoDecimal.Remove(strMeterX_NoDecimal.Length - 1);
+
+            // piece all the unique_id fields together
+            return grid1mil + grid100k + strMeterX_NoDecimal + strMeterY_NoDecimal;
         }
 
-        internal static bool CanOnClearPartsButtonClick
+        private static string GetUSNGID_Line(Row segmentRow)
+        {
+            Polyline line = ((Feature)segmentRow).GetShape() as Polyline;
+            MapPoint midpoint = GeometryEngine.Instance.MovePointAlongLine(line, 0.5, true, 0, SegmentExtension.NoExtension);
+
+            return GetUSNGID_Point(midpoint);
+        }
+
+        internal static bool CanOnCancelButtonClick
         {
             get
             {
-                return Parts.Count > 0;
+                return tempSegmentIDs.Count > 0;
             }
         }
 
-        internal static void OnClearPartsButtonClick()
+        internal static void Reset()
         {
-            Parts.Clear();
-            RaiseOnAddPart(_this, new OnAddPartArgs(Parts.Count));
+            tempSegmentIDs.Clear();
+            currentPart = 1;
+        }
+
+        internal static void OnCancelButtonClick()
+        {
+            Reset();
+
+            QueuedTask.Run(() =>
+            {
+                EditOperation operation = new EditOperation();
+                operation.Name = "Cancel New Route";
+                FeatureClass featureClass = TempSegmentsLayer.GetFeatureClass();
+
+                operation.Callback(context =>
+                {
+                    featureClass.DeleteRows(new QueryFilter());
+                    context.Invalidate(featureClass);
+                }, featureClass);
+
+                bool success = operation.Execute();
+
+                if (!success)
+                {
+                    MessageBox.Show("Error cancelling new route!");
+                }
+            });
         }
 
         #region Overrides
@@ -228,18 +353,5 @@ namespace TrailsAddin
         }
 
         #endregion Overrides
-    }
-
-    public class OnAddPartArgs : EventArgs
-    {
-        public OnAddPartArgs(int num)
-        {
-            numParts = num;
-        }
-        private int numParts;
-        public int NumParts
-        {
-            get { return numParts; }
-        }
     }
 }
---
+++ TrailsAddin/ToggleSelectCheckbox.cs
@@ -0,0 +1,24 @@
+﻿using ArcGIS.Desktop.Framework.Threading.Tasks;
+
+namespace TrailsAddin
+{
+    internal class ToggleSelectCheckbox : ArcGIS.Desktop.Framework.Contracts.CheckBox
+    {
+        public ToggleSelectCheckbox()
+        {
+            IsChecked = false;
+            Enabled = true;
+        }
+
+        protected override void OnClick()
+        {
+            Main.BuildOnSelect = (bool)IsChecked;
+
+            QueuedTask.Run(() =>
+            {
+                Main.SGIDTrailsLayer.ClearSelection();
+                Main.OnCancelButtonClick();
+            });
+        }
+    }
+}
